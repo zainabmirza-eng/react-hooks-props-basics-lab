2ac8acc13bb651a8f3dae33e7ce61c6f40630bc9
zainab mirza <zainabmirzafatah99@gmail.com>
2024-03-31 13:46:02 +0300

    added

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,21 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(props) {
+  let bioLab = "";
+  if (props.bio && props.bio !== null){
+    bioLab = <p>{props.bio}</p>
+  }
+  else {
+    return null;
+  }
+
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <>{bioLab}</>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={props.github} linkedin={props.linkedin} />
     </div>
   );
 }
--- src/components/App.js
+++ src/components/App.js
@@ -11,8 +11,9 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home color={user.color} name={user.name} city={user.city}/>
+      <About bio= {user.bio} github = {user.links.github} linkedin={user.links.linkedin} />
+
     </div>
   );
 }
